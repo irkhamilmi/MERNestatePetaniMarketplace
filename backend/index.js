cad2e28ad4f166ad79a5f5b3fe478b3f9a5ce496
irkhamilmi <irhamhilmi251219@gmail.com>
2024-12-19 01:32:43 +0700

    Sign Up api route

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,9 @@
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+
 dotenv.config();
 
 mongoose
@@ -14,6 +17,11 @@ mongoose
 
 const app = express();
 
+app.use(express.json());
+
 app.listen(3000, () => {
   console.log("server 3000 jalan");
 });
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
