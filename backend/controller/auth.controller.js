3fbee336639073275d1fc2fee7ec41902487c3c5
irkhamilmi <irhamhilmi251219@gmail.com>
2024-12-20 00:36:48 +0700

    middleware daan function handle error

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,8 +1,15 @@
 import User from "../models/user.model.js";
+import bcryptjs from "bcryptjs";
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req, res) => {
+export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
-  const newUser = new User({ username, email, password });
-  await newUser.save();
-  res.status(201).json("json create succes");
+  const hasPasswd = bcryptjs.hashSync(password, 10);
+  const newUser = new User({ username, email, password: hasPasswd });
+  try {
+    await newUser.save();
+    res.status(201).json("json create succes");
+  } catch (error) {
+    next(errorHandler(550, "error from the function"));
+  }
 };
