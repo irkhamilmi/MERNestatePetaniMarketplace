fbaceae0e2df81d13cb57e20fc405e3abe3cbbd9
irkhamilmi <irhamhilmi251219@gmail.com>
2024-12-21 00:46:53 +0700

    sign api route

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,6 +1,7 @@
 import User from "../models/user.model.js";
 import bcryptjs from "bcryptjs";
 import { errorHandler } from "../utils/error.js";
+import jwt from "jsonwebtoken";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -11,5 +12,23 @@ export const signup = async (req, res, next) => {
     res.status(201).json("json create succes");
   } catch (error) {
     next(errorHandler(550, "error from the function"));
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const vadlidUser = await User.findOne({ email });
+    if (!vadlidUser) return next(errorHandler(404, "User not found"));
+    const validPassword = bcryptjs.compareSync(password, vadlidUser.password);
+    if (!validPassword) return next(errorHandler(401, "Passwd salh"));
+    const token = jwt.sign({ id: vadlidUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = vadlidUser._doc;
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
   }
 };
